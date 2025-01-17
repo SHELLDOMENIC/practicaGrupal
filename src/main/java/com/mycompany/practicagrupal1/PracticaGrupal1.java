f3473228299281715664b5e74b808cbfb65ef619
ShellDomenic <domenicaloor177@gmail.com>
2025-01-17 15:20:38 -0500

    Start

---
+++ src/main/java/com/mycompany/practicagrupal1/PracticaGrupal1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagrupal1;
+
+/**
+ *
+ * @author and_j
+ */
+public class PracticaGrupal1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
